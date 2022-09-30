f184b0e1e47d5df08eccbdcf95b96ad121b50447
Christo Kolander <christokolander@hotmail.com>
2022-09-30 17:55:29 +0200

    Removed unused Usings. Added CartStringId and UserStringId where needed. Added migration and updated database.

--- Shopping.Api/Migrations/20220930155403_AddedPropertiesUsernCart.Designer.cs
+++ Shopping.Api/Migrations/20220930155403_AddedPropertiesUsernCart.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shopping.Api.Data;
 
 namespace Shopping.Api.Migrations
 {
     [DbContext(typeof(ShoppingDbContext))]
-    partial class ShoppingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220930155403_AddedPropertiesUsernCart")]
+    partial class AddedPropertiesUsernCart
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Shopping.Api/Migrations/20220930155403_AddedPropertiesUsernCart.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shopping.Api.Migrations
+{
+    public partial class AddedPropertiesUsernCart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserStringId",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CartStringId",
+                table: "Carts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserStringId",
+                table: "Carts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CartStringId",
+                table: "CartItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserStringId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CartStringId",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "UserStringId",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "CartStringId",
+                table: "CartItems");
+        }
+    }
+}
