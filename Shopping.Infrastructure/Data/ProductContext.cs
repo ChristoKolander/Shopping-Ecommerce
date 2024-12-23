ca3612bbff49fc76be15a5f387dfd843ead0a636
Christo Kolander <christokolander@hotmail.com>
2024-12-23 08:25:49 +0100

    Images restored.

--- Shopping.Infrastructure/Data/ProductContext.cs
+++ Shopping.Infrastructure/Data/ProductContext.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Shopping.Core.Entities;
 using Shopping.Core.Entities.People;
+using System;
 using System.Reflection;
+using System.Threading;
 
 
 namespace Shopping.Infrastructure.Data
@@ -20,9 +22,10 @@ namespace Shopping.Infrastructure.Data
 
         public DbSet<Customer> Customers { get; set; }
 
-        // Note: EF CORE 8 Feature for JSON inside SQL
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // JSON column and Arrays in SQL           
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Customer>()
                 .OwnsOne(customer => customer.Details, builder =>
@@ -31,9 +34,30 @@ namespace Shopping.Infrastructure.Data
                     builder.OwnsMany(b => b.PhoneNumbers);
                     builder.ToJson();
                 });
-            
+
+            // Shadow Property
+            modelBuilder.Entity<Product>()
+                        .Property<DateTime>("LastUpdated");
+
+
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
+        }
+
+        // Because of using Shadow Property, we override ( But not to be used inside large batches/loops )
+        // Need to clear from tracking inside these loops, perf. issue.
+        public override int SaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (entry.State == EntityState.Added || entry.State == EntityState.Modified)
+                {
+                    entry.Property("LastUpdated").CurrentValue = DateTime.UtcNow;
+                }
+            }
+            return base.SaveChanges();
         }
     }
 }
